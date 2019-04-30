fb9f82fdb57519f79a4314acd7bf2ce1c0a758d7
Alex <aocc0001@student.monash.edu>
2019-05-01 09:57:47 +1000

    Added rocket items to main screen

--- src/game/Application.java
+++ src/game/Application.java
@@ -7,6 +7,7 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.FancyGroundFactory;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Player;
 import edu.monash.fit2099.engine.World;
 
@@ -40,6 +41,15 @@ public class Application {
 		gameMap.addActor(grunt, 0, 0);
 		Grunt grunt2 = new Grunt("Norbert", player);
 		gameMap.addActor(grunt2,  10, 10);
+		
+		// Testing Rocket Plans, Engine and Body
+		// Will move to appropriate locations later
+		Item rocketPlans = new Item("Rocket Plans", 'P');
+		gameMap.addItem(rocketPlans, 8, 8);
+		Item rocketEngine = new Item("Rocket Engine", 'E');
+		gameMap.addItem(rocketEngine, 8, 9);
+		Item rocketBody = new Item("Rocket Body", 'B');
+		gameMap.addItem(rocketBody, 10, 8);
 		
 		// Testing Implementation of Goon
 		Goon goon = new Goon("Gooney", player);
